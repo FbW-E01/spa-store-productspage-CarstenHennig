f3c520d21300d6f56c50707753a8b6bd870de06c
CarstenHennig <carsten.hennig@gmail.com>
2021-08-16 14:04:47 +0200

    On the way

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import { useProductsContext } from '../contexts/ProductsContext';
+import { useLocaleContext } from '../contexts/LocaleContext';
 
 export default function Header() {
     const [ products ] = useProductsContext();
+    const [ locale, setLocale ] = useLocaleContext();
+
+
+    function changeCurrencyToGBP() {
+        setLocale("en-gb");
+    } 
+
+    function changeCurrencyToUSD() {
+        setLocale("en-us");
+    }
 
     return (
         <header>
             <h1>Product manager</h1>
             <p>Now with {products.length} products</p>
+            <p>Currency is now { locale } </p>
+            <button onClick={changeCurrencyToGBP}>Change currency to GBP</button>
+            <button onClick={changeCurrencyToUSD}>Change currency to USD</button>   
         </header>
     )
-}
+}0
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,13 @@
 import { useProductsContext } from '../contexts/ProductsContext';
+import { useLocaleContext } from '../contexts/LocaleContext';
 
 export default function Products() {
     const [ products ] = useProductsContext();
+    const [ locale ] = useLocaleContext();
 
     return (
         <div className="Products">
-            {products.map(prod => <div>{prod.name}: {prod.price} &euro;</div>)}
+            {products.map(prod => <div>{prod.name}: {prod.price} { locale } </div>)}
         </div>
     )
 }
